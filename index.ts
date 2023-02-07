ecf1383981bbb8aa5c8f2a206155ee0c96274cf6
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2023-02-07 23:34:08 +0000

    Fixed embed indent

--- index.ts
+++ index.ts
@@ -244,10 +244,10 @@ app.get("/:address", async (req, res) => {
                 <meta content="IP: ${address}" property="og:title" />
                 <meta content="Powered by NeuralNexus.dev" property="og:site_name">
                 <meta property="og:description" content="
-                        ${motdtext}
-                        Players: ${players}
-                        Version: ${version}
-                        "/>
+                ${motdtext}
+                Players: ${players}
+                Version: ${version}
+                "/>
                 <meta content="https://api.neuralnexus.dev/api/mcstatus/${addressStr}" property="og:url" />
                 <meta content="https://api.neuralnexus.dev/api/mcstatus/icon/${addressStr}" property="og:image" />
                 <meta content="#7C0014" data-react-helmet="true" name="theme-color" />
